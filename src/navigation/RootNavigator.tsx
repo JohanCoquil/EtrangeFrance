7a636d16f62a53fcfbabefc97f635f4d981d1bdd
JohanCoquil <johan.coquil@gmail.com>
2025-08-08 11:12:00 +0200

    Register profession selection screen

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigator from './TabNavigator';
 import AuthScreen from '../screens/AuthScreen';
 import CreateCharacterScreen from '../screens/CreateCharacterScreen';
+import ChooseProfessionScreen from '../screens/ChooseProfessionScreen';
 import { RootStackParamList } from './types';
 import DeckScreen from '../screens/DeckScreen';
 import CardDrawScreen from '../screens/CardDrawScreen';
@@ -14,6 +15,7 @@ export default function RootNavigator() {
       <Stack.Screen name="MainTabs" component={TabNavigator} />
       <Stack.Screen name="Auth" component={AuthScreen} />
       <Stack.Screen name="CreateCharacter" component={CreateCharacterScreen} />
+      <Stack.Screen name="ChooseProfession" component={ChooseProfessionScreen} />
       <Stack.Screen name="Deck" component={DeckScreen} />
       <Stack.Screen name="CardDraw" component={CardDrawScreen} />
     </Stack.Navigator>
