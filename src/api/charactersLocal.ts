138b347327656145b2211b4986c7fbc88a70eedb
JohanCoquil <johan.coquil@gmail.com>
2025-08-25 13:59:16 +0200

    fix cards deck

--- src/api/charactersLocal.ts
+++ src/api/charactersLocal.ts
@@ -27,7 +27,7 @@ export function useAddCharacter() {
             ${character.charisme}, ${character.memoire}, ${character.volonte}, ${character.sante}, ${character.degats});
         `);
 
-      const defaultCards = "1;2;3;4;5;6;7;8;9;10";
+      const defaultCards = "A;2;3;4;5;6;7;8;9;10";
       const figures = ["Carreau", "Coeur", "Trèfle", "Pique"];
       const randomFigure = figures[Math.floor(Math.random() * figures.length)];
       await db.runAsync(
